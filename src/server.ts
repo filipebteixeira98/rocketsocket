f2aa8453b4d5dd062f4cac2e5c4016cd4f238824
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-06-09 17:54:58 -0300

    feat: add UI template & configure http/websocket

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import path from 'path';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+io.on('connection', (socket) => {
+  console.log('Socket ID: ', socket.id);
+});
 
 app.get('/', (request, response) => {
   return response.json({ message: true });
 });
 
-app.listen(3333, () => {
+server.listen(3333, () => {
   console.log('🚀 Server is running on http://localhost:3333');
 });
