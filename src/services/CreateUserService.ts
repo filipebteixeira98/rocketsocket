98ace7e36be2e44a2bbd123edea0e40811a17e59
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-06-11 02:38:13 -0300

    feat: add CreateUserService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,41 @@
+import { injectable } from 'tsyringe';
+
+import { User } from '../schemas/User';
+
+interface CreateUserDTO {
+  socket_id: string;
+  name: string;
+  email: string;
+  avatar: string;
+}
+
+@injectable()
+class CreateUserService {
+  async execute({ socket_id, name, email, avatar }: CreateUserDTO) {
+    const userAlreadyExists = await User.findOne({ email }).exec();
+
+    if (userAlreadyExists) {
+      const user = await User.findOneAndUpdate(
+        {
+          _id: userAlreadyExists._id,
+        },
+        {
+          $set: { socket_id, name, avatar },
+        }
+      );
+
+      return user;
+    } else {
+      const user = await User.create({
+        socket_id,
+        name,
+        email,
+        avatar,
+      });
+
+      return user;
+    }
+  }
+}
+
+export { CreateUserService };
